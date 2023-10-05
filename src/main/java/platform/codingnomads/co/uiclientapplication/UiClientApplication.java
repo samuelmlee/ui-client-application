c368c42b04f53efc5c6ebd83c0ac9903c9726590
sam <sam.leefr@gmail.com>
2023-10-05 14:52:58 +0200

    initial commit

---
+++ src/main/java/platform/codingnomads/co/uiclientapplication/UiClientApplication.java
@@ -0,0 +1,25 @@
+package platform.codingnomads.co.uiclientapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class UiClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiClientApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
+    }
+
+}
---
+++ src/test/java/platform/codingnomads/co/uiclientapplication/UiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package platform.codingnomads.co.uiclientapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
